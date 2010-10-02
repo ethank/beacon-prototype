b4028f7d4f864d326172c195618567273cb0115c
Ethan Kaplan <ethan.kaplan@wmg.com>
2010-10-01 17:00:24 -0700

    object definitions

--- web/js/script.js
+++ web/js/script.js
@@ -2,9 +2,44 @@
 
 */
 
+function ActivityQueue() {
+    this.activities =- new Array();
+    
+    this.ActivityQueue =function() {
+        this.activities = new Array();
+    }
+    
+    this.addToQueue =  function(activity) {
+        this.activities.push(activity);
+    }
+    
+    this.getFromQueue = function() {
+        return this.activities.pop();
+    }
+    
+}
+
+function Activity() {
+    this.id = '';
+    this.activity = '';
+
+    
+    
+    this.Activity = function() {
+        this.datetime = new Date.getTime();
+    }
+    
+    this.getActivity = function() {
+        var activity = {};
+        activity['id'] = this.id;
+        activity['activity'] = this.activity;
+        activity['timedate'] = this.datetime;
+        return activity;
+    }
+}
 
 $(document).ready(function() {
-    
+    var activityStore = new ActivityQueue();
     var map = 
         {
             'testtag':'test activity',
@@ -15,9 +50,18 @@ $(document).ready(function() {
     log('document ready');
 
     $.each(map, function(key,value) {
-        $('#'+key).click(function() {log(value);});
+        $('#'+key).click(function() {
+            var activity = new Activity();
+            activity.id = key;
+            activity.activity = value;
+            activityStore.addToQueue(activity.getActivity());
+            
+            
+            });
         
     });
+    
+    
     
 
 
