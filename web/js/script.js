add4b24b62f2eac8dacabfae0637ee0ac68839bd
Ethan Kaplan <ethan.kaplan@wmg.com>
2010-10-04 18:31:39 -0700

    got some of the client code working

--- web/js/script.js
+++ web/js/script.js
@@ -2,41 +2,48 @@
 
 */
 
+//Copyright (c) 2010 Nicholas C. Zakas. All rights reserved.
+//MIT License
+
+
+
+// Activity Queue Object
 function ActivityQueue() {
-    this.activities =- new Array();
-    
-    this.ActivityQueue =function() {
-        this.activities = new Array();
-    }
-    
-    this.addToQueue =  function(activity) {
-        this.activities.push(activity);
-    }
-    
-    this.getFromQueue = function() {
-        return this.activities.pop();
-    }
+    EventTarget.call(this);
+    this.activities = new Array();
     
 }
 
+
+ActivityQueue.prototype = new EventTarget();
+ActivityQueue.prototype.constructor = ActivityQueue;
+
+
+ActivityQueue.prototype.addToQueue =  function(activity) {
+    this.activities.push(activity);
+    this.fire({type:"activity_pushed",activity:activity});
+}
+
+ActivityQueue.prototype.getFromQueue = function() {
+    return this.activities.pop();
+}
+
+// Activity Object
 function Activity() {
     this.id = '';
     this.activity = '';
-
+    this.datetime = new Date().getTime();
     
-    
-    this.Activity = function() {
-        this.datetime = new Date.getTime();
-    }
-    
-    this.getActivity = function() {
-        var activity = {};
-        activity['id'] = this.id;
-        activity['activity'] = this.activity;
-        activity['timedate'] = this.datetime;
-        return activity;
-    }
 }
+
+Activity.prototype.getActivity = function() {
+    var activity = {};
+    activity['id'] = this.id;
+    activity['activity'] = this.activity;
+    activity['timedate'] = this.datetime;
+    return activity;
+}
+
 
 $(document).ready(function() {
     var activityStore = new ActivityQueue();
@@ -49,6 +56,8 @@ $(document).ready(function() {
     
     log('document ready');
 
+    
+    // Event binding for the tagged ID's
     $.each(map, function(key,value) {
         $('#'+key).click(function() {
             var activity = new Activity();
@@ -61,7 +70,15 @@ $(document).ready(function() {
         
     });
     
+
     
+    
+    // activity added listener    
+    activityStore.addListener("activity_pushed",function(event) {
+        log("activity " + event['activity'].id);
+        $('#events').prepend('<span style="display:none">'+event['activity'].timedate + ' ' +event['activity'].id+'</span><br/>');
+        $('#events').children(':first').fadeIn('slow');
+    })
     
 
 
