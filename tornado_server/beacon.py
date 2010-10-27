829a18f769c85dbc5d30833147a2cc90620798e5
Ethan Kaplan <ethan.kaplan@wmg.com>
2010-10-27 16:53:53 -0700

    async proc additions

--- tornado_server/beacon.py
+++ tornado_server/beacon.py
@@ -13,7 +13,9 @@ import tornado.web
 import simplejson
 from tornad_io import SocketIOHandler
 from tornad_io import SocketIOServer
-import pickle
+from multiprocessing import Pool, Queue
+import time
+
 
 participants = set()
 
@@ -25,15 +27,28 @@ class IndexHandler(tornado.web.RequestHandler):
         self.render("index.html")
 
 
+def somefunc():
+    time.sleep(5)
+    return os.getpid()
+
 class BeaconHandler(SocketIOHandler):
+    @tornado.web.asynchronous
     def on_open(self,*args,**kwargs):
+       
+        participants.add(self)
+        p = self.application.settings.get('pool')
+        p.apply_async(somefunc,callback = self.async_callback(self.on_getpid))
+    
+    def on_getpid(self,pid):
+        self.send(str(pid))
         data = {}
         data['session'] = self.session.id
         self.send(simplejson.dumps(data))
-        participants.add(self)
     
     def on_message(self,message):
         print message
+        if (message == "test"):
+            self.send("got your test")
             
     def on_close(self):
         participants.remove(self)
@@ -47,7 +62,7 @@ application = tornado.web.Application(
     [(r"/", IndexHandler), beaconRoute], 
     enabled_protocols = ['websocket', 'flashsocket', 'xhr-multipart', 'xhr-polling'],
     flash_policy_port = 8043,
-    socket_io_port = 8889
+    socket_io_port = 8889, pool = Pool(4), queue=Queue()
 )
 
 if __name__ == '__main__':
